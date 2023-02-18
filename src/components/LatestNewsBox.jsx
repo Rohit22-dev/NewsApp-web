c6706b3f071dd74bdd27eba340cabd4a72b4596f
Rohit <rk22012001@gmail.com>
2023-02-19 00:31:36 +0530

    .

--- src/components/LatestNewsBox.jsx
+++ src/components/LatestNewsBox.jsx
@@ -1,15 +1,15 @@
-import empty from '../assets/empty_news.jpeg'
+import empty from "../assets/empty_news.jpeg";
 
 const LatestNewsBox = ({ data }) => {
   return (
     <div className="m-1 border border-gray-400 rounded-lg flex overflow-hidden line">
-      {data.image_url ? (
-        <img src={data.image_url} width={70} />
-      ) : (
-        <img src={empty} width={70} />
-      )}
+      <div className="h-80 w-80">
+        {data.image_url ? <img src={data.image_url} /> : <img src={empty} className='object-cover'/>}
+      </div>
 
-      <p className="p-1 tracking-tighter leading-normal">{data.title}</p>
+      <p className="p-1 tracking-tighter leading-normal line-clamp-2">
+        {data.title}
+      </p>
     </div>
   );
 };
