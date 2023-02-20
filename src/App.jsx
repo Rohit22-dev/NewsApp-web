b562078ad6b70cb725175be61f0eb80cb7ad1031
Rohit <rk22012001@gmail.com>
2023-02-20 17:47:09 +0530

    Added detailed news box data

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
       }
     };
     func();
-    console.log(Data.newsExpand);
+    // console.log(Data.newsExpand);
   },[Data.search]);
   return (
     <UserContext.Provider value={{ Data, setData }}>
--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -10,7 +10,7 @@ const NewsCard = ({ data }) => {
   return (
     <div className="h-52 w-full overflow-hidden rounded-md hover:shadow-lg hover:shadow-[#c21636] shadow-black shadow-lg hover:animate-wiggle cursor-pointer relative flex items-end" onClick={()=>handleClick()}>
       <img src={data.urlToImage} className="h-full w-full object-fill" />
-      <div className=" text-neutral-300 absolute w-full h-full bg-gradient-to-b from-transparent to-[#000000d0]  flex p-2 items-end justify-start leading-4 ">{data.title}</div>
+      <div className="flex text-neutral-300 absolute w-full h-full bg-gradient-to-b from-transparent to-[#000000d0] p-2 items-end justify-start leading-4 ">{data.title}</div>
     </div>
   );
 };
